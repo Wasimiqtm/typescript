26a0233f58cf8238bd4f4a90bee3792539582756
wasim939 <wasim.iqtm@gmail.com>
2024-04-03 23:41:45 +0500

    step 3 completed

---
+++ step03c_import_inquirer_ECMAScript_module/app.js
@@ -0,0 +1,9 @@
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+    {
+        name: "age",
+        type: "number",
+        message: "Enter your age",
+    }
+]);
+console.log(`Inshaa Allah, after ${60 - answers.age} you will of 60`);
---
+++ step03d_chalk/app.ts
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let answers = await inquirer.prompt([
+    {
+        name: "age",
+        type: "number",
+        message: "Enter age"
+    }
+])
+
+console.log(chalk.blue(`Inshaa Allah, after ${60 - answers.age} you will of 60`))
